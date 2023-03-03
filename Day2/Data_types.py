a91c77133dc009ee63c1227e0383b0e4bcf12419
alexkimbi <alexkimbi@hotmail.com>
2023-03-03 00:32:48 -0500

    Day 2 files

---
+++ Day2/Data_types.py
@@ -0,0 +1,19 @@
+#Data types
+
+#Strings
+print("Hello"[0])  # This will print the first later which s "H"  as the index starts counting from 0.
+
+#Integer
+print(123 + 345)
+
+print(123_345_345)  # Here the numbers will be printed as a whole without the underscore 
+
+#Float
+
+33.345  # floats are numbers with decimal points
+
+#Boolean    : This also have two possible values, (TRUE or FALSE) 
+True
+False
+
+
---
+++ Day2/integer_data_type.py
@@ -0,0 +1,3 @@
+num_char = len(input("What is your name?")) # This variable provides the length of the name which will be in numbers making it "integers"
+new_num_char = str(num_char)                #This will convert  the num_char variable from  int to string with the use of "str"
+print("Your name has " + new_num_char + " Characters.")
