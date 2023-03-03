c671e106ef6df14c36843fe6cddeccd47039415d
alexkimbi <alexkimbi@hotmail.com>
2023-03-03 00:39:24 -0500

    reorganized the comment inthe interger file

--- Day2/integer_data_type.py
+++ Day2/integer_data_type.py
@@ -1,3 +1,5 @@
-num_char = len(input("What is your name?")) # This variable provides the length of the name which will be in numbers making it "integers"
-new_num_char = str(num_char)                #This will convert  the num_char variable from  int to string with the use of "str"
+# This variable provides the length of the name which will be in numbers making it "integers"
+num_char = len(input("What is your name?\n")) 
+#This will convert  the num_char variable from  int to string with the use of "str"
+new_num_char = str(num_char)  
 print("Your name has " + new_num_char + " Characters.")
