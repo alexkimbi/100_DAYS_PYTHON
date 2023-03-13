859a3d7c339fd2a1b7b67e434935f0020ab6fee8
alexkimbi <alexkimbi@hotmail.com>
2023-03-13 14:04:36 -0400

    Added the updated files

--- Day2/tip_calculator.py
+++ Day2/tip_calculator.py
@@ -1,6 +1,10 @@
 print("Welcome to the tip calculator")
-total_bill = input("What was your your total bill? " "")
-percentage = input("What percentage of your bill do you want to pay as tip? 10, 12, or 15? " "")
-number_of_people = input("How many people do you want to split the bill? " "")
-tip_to_be_paid = (f"({percentage} // {100} ) * ({total_bill})")
-print(f"Each person should pay a total bill of : {tip_to_be_paid} ")
+bill = float(input("What was your your total bill?  $ " ""))
+tip_percentage = int(input("What percentage of your bill do you want to pay as tip? 10, 12, or 15? " ""))
+number_of_people = int(input("How many people do you want to split the bill? " ""))
+tip_to_be_paid = tip_percentage * tip_percentage
+bill_per_person = tip_to_be_paid / number_of_people
+total_bill = tip_percentage / 100 * bill + bill
+final_bill = round(bill_per_person, 2)
+print(f"The total bill to be paid including tips is : {total_bill}")
+print(f"The total bill per person {final_bill}")
