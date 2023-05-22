72d7b8af81ee743c20e7911b8c9225aacfe13fbf
alexkimbi <39074598+alexkimbi@users.noreply.github.com>
2023-05-21 21:33:13 -0500

    Update day1_python.py

--- Day1/day1_python.py
+++ Day1/day1_python.py
@@ -1,6 +1,6 @@
 Lname = input("What is your Last name?\n")
 Fname = input("What is yur first name?\n")
-print ("I was told your name appear in this order "  + Fname +  " " + Lname)
+print ("I was told your name appear in this order of  "  + Fname +  " " + Lname)
 # length = len(Lname)
 # length = len(Fname)
 
