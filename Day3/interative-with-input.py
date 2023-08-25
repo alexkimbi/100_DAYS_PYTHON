b301d19a4f9fbe4d3a54e91d43b8d6716095f48d
Alex Kimbi <alexkimbi@hotmail.com>
2023-08-25 12:55:58 -0500

    Day1

--- Day3/interative-with-input.py
+++ Day3/interative-with-input.py
@@ -1,4 +1,11 @@
 #!/usr/binpython3
 name = input("What is the name you are talking about?")
-             print(name)
+print("You just told me your name is "  +  name.upper())
+if name == "song":
+    print ("you are good to go")
+else:
+    print ("you got it wrong")
+
+
+
            
